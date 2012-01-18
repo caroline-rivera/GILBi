81601c08f957e6a85f3fec65d1d0417e57829d16
Caroline <caroline.rivera@hotmail.com>
2012-01-17 23:39:30 -0200

    Client-side functions to handle Purchase Orders.

--- media/js/custom/purchase_order.js
+++ media/js/custom/purchase_order.js
@@ -5,12 +5,14 @@ var PurchaseOrder = {};
 PurchaseOrder.Selectors = {
 	
 	Buttons: {
-		add: '#button_add_book'
+		add: "#button_add_book",
+		accept: "#button_accept_book",
+		remove: "#button_remove_item"
 	},
 	
 	Tables: {
-		orders: '#table_orders',
-		purchaseItems: '#table_purchase_items'
+		orders: "#table_orders",
+		purchaseItems: "#table_purchase_items"
 	}
 }
 
@@ -19,8 +21,8 @@ PurchaseOrder.Selectors = {
 PurchaseOrder.Functions = {
 
 //------------------------------------------------------------------------- init	
-	init: function()
-	{
+	init: function() {
+		
 		var btn = PurchaseOrder.Selectors.Buttons;
 								
 		/* Funcoes para a tab1: Novo Pedido de Compra */
@@ -28,66 +30,20 @@ PurchaseOrder.Functions = {
 		PurchaseOrder.Functions.createOrdersTable();
 		PurchaseOrder.Functions.createPurchaseItemsTable();
 		
-		var $grid = $(PurchaseOrder.Selectors.Tables.purchaseItems);
-				
-		$(btn.add).click( function() {
-			//PurchaseOrder.Functions.searchOrdersFn();
-			
-			// 'quantity' eu peguei da tela
-			var $quantity = $("#id_quantity"),
-			    quantity = $quantity.val();
-			
-			// 'book' voltou do servidor
-			var book = {
-				"id": 1,
-				"name": "Livro legal",
-				"author": "Alex",
-				"spiritualAuthor": "Carol",
-				"publisher": "A Editora"
-			};
-			
-			var purchaseItemRow = {
-				"bookId": book.id,
-				"bookName": book.name,
-				"author": book.author,
-				"spiritualAuthor": book.spiritualAuthor,
-				"publisher": book.publisher,
-				"quantity": quantity
-			};
-			
-			var purchaseItem = {
-				
-				"bookId": book.id,
-				"bookOrders": [],
-				"quantity": quantity
-			};
-
-			var rowData = $grid.jqGrid("getRowData", purchaseItemRow.bookId);
-			
-			if (jQuery.isEmptyObject(rowData)) {
-			
-				$grid.jqGrid("addRowData", purchaseItemRow.bookId, purchaseItemRow, "first");
-				             
-			} else {
-			
-				$grid.jqGrid("setRowData",
-				             purchaseItemRow.bookId,
-				             { "quantity": (+rowData.quantity) + (+quantity) });
-				
-			}
-				
-		});		
+		$(btn.add).click(PurchaseOrder.Functions.addBookToPurchaseOrder);
+		$(btn.accept).click(PurchaseOrder.Functions.addBookOrderToPurchaseOrder);
+		$(btn.remove).click(PurchaseOrder.Functions.removeItemFromPurchaseOrder);
 	},
 
 //------------------------------------------------------------------------- createOrdersTable
-	createOrdersTable: function()
-	{	
+	createOrdersTable: function() {
+			
 		
 	},
 	
 //------------------------------------------------------------------------- createPurchaseItemsTable
-	createPurchaseItemsTable: function()
-	{	
+	createPurchaseItemsTable: function() {
+			
 		var grid = $(PurchaseOrder.Selectors.Tables.purchaseItems),
 		    columnsTitles = ['Livro', 'Autor(es)', 'Autor(es) Espiritual(ais)','Editora', 
 		    			     'Quantidade'],
@@ -107,9 +63,149 @@ PurchaseOrder.Functions = {
 		    multiselect: true,
 		    sortname: 'name',
 		    viewrecords: true,
-		    caption:'Itens do Pedido de Compra'				
+		    caption:'Itens do Pedido de Compra',
+		    userdata: {
+				purchaseItems: []
+			}			
 		});
 	},
+	
+//-------------------------------------------------------------------------- addBookToPurchaseOrder
+	addBookToPurchaseOrder: function(event) {
+		
+		// 'quantity' eu peguei da tela
+		var $quantity = $("#id_quantity"),
+		    quantity = $quantity.val();
+		
+		// 'book' voltou do servidor
+		var book = {
+			"id": 1,
+			"name": "Livro legal",
+			"author": "Alex",
+			"spiritualAuthor": "Carol",
+			"publisher": "A Editora"
+		};		
+		
+		PurchaseOrder.Functions._addItemToPurchaseOrder(book, quantity);
+	},
+	
+//--------------------------------------------------------------------- addBookOrderToPurchaseOrder
+	addBookOrderToPurchaseOrder: function(event) {
+		
+		// 'quantity' veio da grid
+		var quantity = 3;
+		
+		// 'book' veio da grid
+		var book = {
+			"id": 3,
+			"name": "Livro legal",
+			"author": "Alex",
+			"spiritualAuthor": "Carol",
+			"publisher": "A Editora"
+		};
+		
+		// 'orderId' veio da grid
+		var bookOrderId = 20;
+		
+		PurchaseOrder.Functions._addItemToPurchaseOrder(book, quantity, bookOrderId);
+	},
+	
+//-------------------------------------------------------------------------- addItemToPurchaseOrder
+	_addItemToPurchaseOrder: function(book, quantity, bookOrderId) {
+			
+		var $grid = $(PurchaseOrder.Selectors.Tables.purchaseItems),
+		
+			purchaseItemRow = {
+				bookId: book.id,
+				bookName: book.name,
+				author: book.author,
+				spiritualAuthor: book.spiritualAuthor,
+				publisher: book.publisher,
+				quantity: quantity
+			},
+			
+			purchaseItem = {
+				bookId: book.id,
+				bookOrdersIds: [],
+				quantity: quantity
+			},
+			
+			userData = $grid.jqGrid("getGridParam", "userdata"),
+			purchaseItems = userData.purchaseItems,
+			
+			rowData = $grid.jqGrid("getRowData", purchaseItemRow.bookId);
+			
+		
+		if (jQuery.isEmptyObject(rowData)) {
+			
+			// Adiciona o item ao modelo
+			if (typeof bookOrderId !== "undefined") {
+				purchaseItem.bookOrdersIds.push(bookOrderId);
+			}
+			
+			purchaseItems.push(purchaseItem);
+		
+			// Adiciona o item à visualização
+			$grid.jqGrid("addRowData", purchaseItemRow.bookId, purchaseItemRow, "first");
+			             
+		} else {
+			
+			var existingItem = 
+					PurchaseOrder.Functions._findPurchaseItemByBookId(purchaseItems,
+			                                                          purchaseItem.bookId).item,
+                newQuantity = (+rowData.quantity) + (+quantity);
+		
+			// Atualiza o item do modelo
+			if (existingItem) {
+				existingItem.quantity = newQuantity;
+				
+				if (typeof bookOrderId !== "undefined") {
+					existingItem.bookOrdersIds.push(bookOrderId);
+				}
+			}
+		
+			// Atualiza o item da visualização
+			$grid.jqGrid("setRowData", purchaseItemRow.bookId, { "quantity": newQuantity });
+		}
+	},
+	
+//--------------------------------------------------------------------- removeItemFromPurchaseOrder
+	removeItemFromPurchaseOrder: function(event) {
+		
+		var $grid = $(PurchaseOrder.Selectors.Tables.purchaseItems),
+		
+			userData = $grid.jqGrid("getGridParam", "userdata"),
+			purchaseItems = userData.purchaseItems,
+			
+			// Faz uma cópia, pra não "confundir" o loop
+			selectedIds = $grid.jqGrid("getGridParam", "selarrrow").slice();
+		
+		
+		for (var i = 0; i < selectedIds.length; i++) {
+			
+			var selectedId = selectedIds[i],
+			    index = PurchaseOrder.Functions._findPurchaseItemByBookId(purchaseItems,
+			                                                              selectedId).index;
+			
+			// Remove o item do modelo                                                             
+			purchaseItems.splice(index, 1);
+			
+			// Remove o item da visualização			
+			$grid.jqGrid("delRowData", selectedId);
+		}
+		
+	},	
+		
+//------------------------------------------------------------------------ findPurchaseItemByBookId
+	
+	_findPurchaseItemByBookId: function(purchaseItems, bookId) {
+	
+		for (var i = 0; i < purchaseItems.length; i++) {
+			if (purchaseItems[i].bookId == bookId) {
+				return { index: i, item: purchaseItems[i] };
+			}
+		}
+	},	
 		
 //------------------------------------------------------------------------- listFn	
 	listFn: function(data, grid)
@@ -121,21 +217,5 @@ PurchaseOrder.Functions = {
             grid.jqGrid('addRowData', i, data[i]);
         }
 		
-	},	
-
-//------------------------------------------------------------------------- getSelectedRowFn		
-	getSelectedRowFn: function(grid)
-	{
-		var selRowNumber = grid.getGridParam('selrow');
-					
-		if (selRowNumber == null)
-		{
-			return '';
-		}
-		else
-		{		
-			var bookId = grid.getRowData(selRowNumber)['id'];
-			return bookId;
-		}
 	}
-}	
+}
