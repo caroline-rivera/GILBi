c521eebed8abeea9c5dd54e7a6376364094f9a45
Caroline <caroline.rivera@hotmail.com>
2012-02-07 07:41:42 -0200

    Setting expiration time of a session.

--- mistrael/helpers/session_helper.py
+++ mistrael/helpers/session_helper.py
@@ -1,11 +1,15 @@
 from gilbi.apps.user_profiles.models import User, Seller, Manager
 
+EXPIRATION_TIME = 1800
+
 def create_session(request, id):
     if 'user_id' not in request.session:
         request.session['user_id'] = id
+        request.session.set_expiry(EXPIRATION_TIME)
 
 def validate_session(request):
     if 'user_id' in request.session and User.objects.filter(id=request.session['user_id']).exists() == True:
+        request.session.set_expiry(EXPIRATION_TIME)
         return True
     else:
         request.session.flush()
@@ -13,12 +17,14 @@ def validate_session(request):
 
 def validate_seller_session(request):
     if Seller.objects.filter(id=request.session['user_id']).exists() == True:
+        request.session.set_expiry(EXPIRATION_TIME)
         return True
     else:
         return False
 
 def validate_manager_session(request):
     if Manager.objects.filter(id=request.session['user_id']).exists() == True:
+        request.session.set_expiry(EXPIRATION_TIME)
         return True
     else:
         return False
